24cb4727f45c235e32d4ffff35f810f7d5f532e2
yusuke nakano <yuusukechrono@gmail.com>
2020-05-27 17:22:41 +0000

    Userサンプル作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,10 @@
 
 require "csv"
 
-CSV.foreach('db/question.csv') do |row|
-  Question.create(:category_id => row[0], :detail => row[1], :answer_left => row[2], :answer_right => row[3])
+#CSV.foreach('db/question.csv') do |row|
+ # Question.create(:category_id => row[0], :detail => row[1], :answer_left => row[2], #:answer_right => row[3])
+#end
+
+CSV.foreach('db/user.csv') do |row|
+  User.create(:last_name => row[0], :first_name => row[1], :email => row[2], :password => row[3], :password_confirmation => row[4], :sex => row[5], :material_status => row[6], :birthday => row[7])
 end
