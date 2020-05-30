b659bbf23921997d0f2bdc2557355c5694271b60
yusuke nakano <yuusukechrono@gmail.com>
2020-05-30 06:01:00 +0000

    プランの過去入力制限

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -5,4 +5,11 @@ class Plan < ApplicationRecord
     plan_date.strftime("%y年%m月%d日")
   end
 
+  validate :pretend_ago
+
+  def pretend_ago()
+      errors.add(:plan_date, '日付が過去です') if plan_date < Date.today
+  end
+
 end
+
